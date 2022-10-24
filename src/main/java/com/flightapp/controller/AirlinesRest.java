91854dda7ea73571c4880d9c673db137afe8b57c
bishwajeet <bishwajeetmandal93@gmail.com>
2022-10-24 16:08:35 +0530

    first commit

---
+++ src/main/java/com/flightapp/controller/AirlinesRest.java
@@ -0,0 +1,19 @@
+package com.flightapp.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/api")
+public class AirlinesRest {
+	
+	@GetMapping("/")
+	public List<String> findAllAirlines() {
+		System.out.println("Inside findAllAirlines");
+		return List.of("Indigo", "Vistara");
+	}
+
+}
